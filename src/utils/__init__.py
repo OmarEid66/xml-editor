05bfefa6dcb7293a7542db4fa5bb35d265351cb3
Your Name <your-email@example.com>
2025-12-16 16:06:04 +0200

    chore: export XMLTree, XMLNode, XMLParseError from utils module

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,7 @@
 from .file_io import read_file, write_file, read_binary, write_binary, pretty_format
 from .token_utils import is_opening_tag, is_closing_tag, extract_tag_name, tokenize
 from .binary_utils import ByteUtils
+from .xml_tree import XMLTree, XMLNode, XMLParseError
 
 __all__ = [
     'read_file',
@@ -13,4 +14,7 @@ __all__ = [
     'extract_tag_name',
     'tokenize',
     'ByteUtils',
+    'XMLTree',
+    'XMLNode',
+    'XMLParseError',
 ]
